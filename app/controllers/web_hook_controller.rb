2718d13eeb79f112638625757f6b215ebedb4f0f
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-03-01 23:06:32 -0800

    test

--- app/controllers/web_hook_controller.rb
+++
@@ -1,6 +0,0 @@
-
-class WebHookController < ApplicationController
-  def init
-    File.write('lib/file.rb', 'test')
-  end
-end
---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,4 @@
+class WebhooksController < ApplicationController
+	def init
+	end
+end
