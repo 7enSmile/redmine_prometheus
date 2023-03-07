53994251beaf2255e4e26c2e10dc63249eff8215
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-03-07 09:50:03 +0700

    Test

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,13 +1,3 @@
 class SettingsController < ApplicationController
-  unloadable
-
-  def update
-    @settings = Setting.plugin_redmine_prometheus
-    @settings['user'] = params[:user]
-    @settings['project'] = params[:project]
-    if request.post? && @settings.save
-      flash[:notice] = l(:notice_successful_update)
-    end
-    redirect_to :action => 'plugin', :id => 'redmine_prometheus'
-  end
+ 
 end
