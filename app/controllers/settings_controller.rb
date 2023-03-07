dd63eb0bcbdd9870438af22233a3d54ca2accb98
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-03-07 11:22:31 +0700

    Update settings_controller.rb

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,8 +2,8 @@ class SettingsController < ApplicationController
   unloadable
   def update
     @settings = Setting.plugin_redmine_prometheus
-    @settings['user'] = params[:user]
-    @settings['project'] = params[:project]
+    @settings['user'] = "123"
+    @settings['project'] = "123"
     @settings.save
 
   end
