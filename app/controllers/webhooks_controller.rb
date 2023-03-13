bcefc41513ca611473e9700d854aa2d046885b0e
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-03-13 10:29:56 +0700

    creating issue from grafana

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,18 @@
 class WebhooksController < ApplicationController
 skip_before_action :verify_authenticity_token
 	def init
-		render json: request.body.read
+		data = JSON.parse(request.body.read)
+		issue = Issue.new
+		issue.project = Project.find_by_id(Setting.plugin_redmine_prometheus[:project])
+		issue.assigned_to = User.find_by_id(Setting.plugin_redmine_prometheus[:user_assignee])
+		issue.subject = "#{data["alerts"][0]["status"]} #{data["alerts"][0]["startsAt"][11, 8]}"
+		issue.description = "Grafana alert. #{data["alerts"][0]["annotations"]["summary"]}"
+		issue.status = IssueStatus.find_by_name("New")
+		issue.priority = IssuePriority.find_by_name("Normal")
+		issue.author = User.find_by_id(1)
+		issue.tracker = Tracker.find_by_name("Support")
+		issue.start_date = data["alerts"][0]["startsAt"][0, 10]
+
+		issue.save
 	end
 end
